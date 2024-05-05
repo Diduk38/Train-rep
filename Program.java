bbc6df4ce253cc27a216b273ba7970ae365bf07f
diduk <diduk38pl@gmail.com>
2024-05-05 15:09:26 +0300

    modifided 1 line Hotfix

--- Program.java
+++ Program.java
@@ -2,7 +2,7 @@ public class Program {
 
     public static void main(String[] args) {
         
-        System.out.println("Hello Git");
+        System.out.println("Hello Git from nazar");
 
         String name="Nazar";
         String surname="Diduk";
